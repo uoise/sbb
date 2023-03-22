2e2aa53b28e96ee282b592ba7a0ec85bde2c4d23
Youngwoo Choi <yw823x@daum.net>
2023-03-22 13:17:13 +0900

    add search(complex query)

--- src/main/java/com/ll/sbb/question/service/QuestionService.java
+++ src/main/java/com/ll/sbb/question/service/QuestionService.java
@@ -1,15 +1,18 @@
 package com.ll.sbb.question.service;
 
 import com.ll.sbb.DataNotFoundException;
+import com.ll.sbb.answer.model.Answer;
 import com.ll.sbb.question.model.Question;
 import com.ll.sbb.question.repository.QuestionRepository;
 import com.ll.sbb.user.model.SiteUser;
+import jakarta.persistence.criteria.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,9 +25,32 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                        cb.like(q.get("content"), "%" + kw + "%"),
+                        cb.like(u1.get("username"), "%" + kw + "%"),
+                        cb.like(a.get("content"), "%" + kw + "%"),
+                        cb.like(u2.get("username"), "%" + kw + "%"));
+            }
+        };
+    }
+
+
     @Transactional
-    public List<Question> getList() {
-        return questionRepository.findAll();
+    public Page<Question> getList(int page, String kw) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return questionRepository.findAllByKeyword(kw, pageable);
     }
 
     @Transactional
